34ff3467b070d7dacdcc9568a93c39f4f9ac362f
Paul Tim <paveltimofeenko@gmail.com>
2020-12-24 20:47:41 +0300

    add database creation files

---
+++ db/DDL.sql
@@ -0,0 +1,16 @@
+use balancer_db;
+
+create table Balancer(
+	ID int auto_increment,
+    Name varchar(10),
+    Primary key(ID)
+);
+
+create table VirtualMachine(
+	ID int auto_increment,
+    BalancerID int not null,
+    isWorking bool not null default 1,
+    Primary key(ID),
+    Foreign key(BalancerID) references Balancer(ID)
+);
+
---
+++ db/DML.sql
@@ -0,0 +1,13 @@
+use balancer_db;
+
+insert into Balancer(Name)
+values
+("balancer1"),
+("balancer2"),
+("balancer3");
+
+insert into VirtualMachine(BalancerID,isWorking)
+values
+(1,1),(1,0),(2,1),(3,1),(1,1),(3,1),(1,1),(3,1),
+(2,1),(1,1),(2,1),(1,1),(2,0),(3,1),(1,0),(2,1),
+(3,1),(2,0),(1,1),(3,0),(1,1),(2,1),(1,1),(3,1);
